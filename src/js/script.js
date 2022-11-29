3cf40adf72f91fdef99b1c2979f9c457002e64da
Anastasiia Myelnykova <slonotop2103@gmail.com>
2022-11-29 17:41:14 +0200

    add files for animation

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,6 @@ const button = document.querySelector(".menu-button");
 const buttonImage = document.getElementById('btn-img');
 
 button.addEventListener('click', (e) => {
-  //можна зробити тут анімацію з додаванням класів тоді меню буде відкриватися красиво, якщо хочеш я тобі з цим допоможу
   if (menu.style.display === 'block') {
     menu.style.display = 'none';
     buttonImage.setAttribute("src", "./dist/img/header/menu-open-button.png");
