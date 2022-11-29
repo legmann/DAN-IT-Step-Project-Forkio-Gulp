00f284d2164aa5e4c2ba3337fb3f061d9aff7ffd
Anastasiia Myelnykova <slonotop2103@gmail.com>
2022-11-29 18:28:06 +0200

    add some  animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,21 @@
 const menu = document.getElementById('menu-mobile');
 const button = document.querySelector(".menu-button");
 const buttonImage = document.getElementById('btn-img');
+const buttonSmallImg = document.querySelector(".menu-button__stick_small");
+const buttonSecondBigImg = document.querySelector(".menu-button__stick_big_2");
+const buttonFirstBigImg = document.querySelector(".menu-button__stick");
 
 button.addEventListener('click', (e) => {
   if (menu.style.display === 'block') {
     menu.style.display = 'none';
     buttonImage.setAttribute("src", "./dist/img/header/menu-open-button.png");
+    buttonSmallImg.classList.add("animation1_reverse")
   } else {
     menu.style.display = 'block';
-    buttonImage.setAttribute("src", "./dist/img/header/menu-close-button.png");
+    buttonSmallImg.classList.add("animation1");
+    buttonSecondBigImg.classList.add("animation3")
+    buttonFirstBigImg.classList.add("animation2")
+    // buttonImage.setAttribute("src", "./dist/img/header/menu-close-button.png");
   }
 });
 
