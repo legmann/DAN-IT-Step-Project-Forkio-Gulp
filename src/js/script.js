d7576712e8f83d493ae629d37e0eded1e4ddb7bd
Anastasiia Myelnykova <slonotop2103@gmail.com>
2022-11-30 16:46:31 +0200

    add animation for button

--- src/js/script.js
+++ src/js/script.js
@@ -3,47 +3,42 @@ const button = document.querySelector(".menu-button");
 const buttonSmallImg = document.querySelector(".menu-button__stick_small");
 const buttonSecondBigImg = document.querySelector(".menu-button__stick_big_2");
 const buttonFirstBigImg = document.querySelector(".menu-button__stick");
-const menuList = document.querySelector("#menu-mobile_list");
 
-
-function closeMenu(){
-  menu.classList.remove("menu_open");
-  buttonSmallImg.classList.add("animation1_reverse");
-  buttonFirstBigImg.classList.add("animation2_reverse");
-  buttonSecondBigImg.classList.add("animation3_reverse");
-  setTimeout(function() {
-    menu.classList.add("menu_open_reverse");
-    buttonSmallImg.classList.remove("animation1");
-    buttonSecondBigImg.classList.remove("animation3");
-    buttonFirstBigImg.classList.remove("animation2");
-  }, 0);
+function closeMenu() {
+    menu.classList.remove("menu_open");
+    buttonSmallImg.classList.add("animation1_reverse");
+    buttonFirstBigImg.classList.add("animation2_reverse");
+    buttonSecondBigImg.classList.add("animation3_reverse");
+    setTimeout(function () {
+        buttonSmallImg.classList.remove("animation1");
+        buttonSecondBigImg.classList.remove("animation3");
+        buttonFirstBigImg.classList.remove("animation2");
+    }, 0);
 }
 
-function openMenu(){
-  buttonSmallImg.classList.remove("animation1_reverse");
-  buttonFirstBigImg.classList.remove("animation2_reverse");
-  buttonSecondBigImg.classList.remove("animation3_reverse");
-  menu.classList.remove("menu_open_reverse");
-  setTimeout(function() {
-    menu.classList.add("menu_open");
-    buttonSmallImg.classList.add("animation1");
-    buttonSecondBigImg.classList.add("animation3");
-    buttonFirstBigImg.classList.add("animation2");
-  }, 0);
+function openMenu() {
+    buttonSmallImg.classList.remove("animation1_reverse");
+    buttonFirstBigImg.classList.remove("animation2_reverse");
+    buttonSecondBigImg.classList.remove("animation3_reverse");
+    setTimeout(function () {
+        menu.classList.add("menu_open");
+        buttonSmallImg.classList.add("animation1");
+        buttonSecondBigImg.classList.add("animation3");
+        buttonFirstBigImg.classList.add("animation2");
+    }, 0);
 }
-
 
 button.addEventListener('click', (e) => {
-  if (menu.classList.contains("menu_open")) {
-    closeMenu();
-  } else {
-    openMenu();
-  }
+    if (menu.classList.contains('menu_open')) {
+        closeMenu();
+    } else {
+        openMenu();
+    }
 });
 document.addEventListener('click', (e) => {
-  const withinBoundaries = e.composedPath().includes(menu) || e.composedPath().includes(button);
-  if (!withinBoundaries) {
-    closeMenu();
-  }
+    const withinBoundaries = e.composedPath().includes(menu) || e.composedPath().includes(button);
+    if (!withinBoundaries) {
+        closeMenu();
+    }
 });
 
